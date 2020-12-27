bb73521986758fdf4fb1f49d27ff5d53a904d1a6
Gavin Stark <atthecodeface@gmail.com>
2020-12-27 11:49:33 +0000

    [animate] Trying to start animating

--- webgl.js
+++ webgl.js
@@ -9,19 +9,22 @@ function main() {
  const vsSource = `
     attribute vec4 aVertexPosition;
     uniform mat4 uModelViewMatrix;
+    uniform mat4 uCameraMatrix;
     uniform mat4 uProjectionMatrix;
-    varying vec4 world_pos;
+    varying vec4 color_pos;
     void main() {
+      color_pos = aVertexPosition;
+      vec4 world_pos;
       world_pos = uModelViewMatrix * aVertexPosition;
-      gl_Position = uProjectionMatrix * world_pos;
+      gl_Position = uProjectionMatrix * uCameraMatrix * world_pos;
     }
   `;
 
   const fsSource = `
     precision mediump float;
-    varying vec4 world_pos;
+    varying vec4 color_pos;
     void main() {
-      gl_FragColor = vec4(world_pos.x/2.0+0.5, world_pos.y/2.0+0.5, world_pos.z/2.0+0.5, 1.0);
+      gl_FragColor = vec4(color_pos.x/2.0+0.5, color_pos.y/2.0+0.5, color_pos.z/2.0+0.5, 1.0);
     }
   `;
 
@@ -34,19 +37,25 @@ function main() {
     },
     uniformLocations: {
       projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      cameraMatrix:     gl.getUniformLocation(shaderProgram, 'uCameraMatrix'),
+      modelViewMatrix:  gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
     },
   };
 
   const buffers = initBuffers(gl);
-  drawScene(gl, programInfo, buffers);
+    run_animation(gl, programInfo, buffers);
+}
+var time=0.0;
+function run_animation(gl, programInfo, buffers) {
+    step_animation = function() {drawScene(gl, programInfo, buffers, time); time+=0.1; requestAnimationFrame(step_animation);}
+    requestAnimationFrame(step_animation);
 }
 
 function initBuffers(gl) {
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  const positions2 = [
+  const positions = [
       1.0,  1.0, 1.0,
       -1.0,  1.0, 1.0,
       1.0, -1.0, 1.0,
@@ -55,13 +64,6 @@ function initBuffers(gl) {
       -1.0,  1.0, -1.0,
       1.0, -1.0, -1.0,
       -1.0, -1.0, -1.0,
-  ];
-
-  const positions = [
-      1.0,  1.0,
-      -1.0,  1.0,
-      1.0, -1.0,
-      -1.0, -1.0,
   ];
 
   gl.bufferData(gl.ARRAY_BUFFER,
@@ -76,7 +78,7 @@ function initBuffers(gl) {
 //
 // Draw the scene.
 //
-function drawScene(gl, programInfo, buffers) {
+function drawScene(gl, programInfo, buffers, time) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -97,29 +99,30 @@ function drawScene(gl, programInfo, buffers) {
   const zFar = 100.0;
   const projectionMatrix = mat4.create();
 
-  // note: glmatrix.js always has the first argument
-  // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
-  // Set the drawing position to the "identity" point, which is
-  // the center of the scene.
-  const modelViewMatrix = mat4.create();
-
-  // Now move the drawing position a bit to where we want to
-  // start drawing the square.
-
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
+  const cameraMatrix = mat4.create();
+  mat4.translate(cameraMatrix,     // destination matrix
+                 cameraMatrix,     // matrix to translate
                  [-0.0, 0.0, -6.0]);  // amount to translate
+
+  const modelViewMatrix = mat4.create();
+  const axis1 = vec3.create();
+  const axis2 = vec3.create();
+  vec3.set(axis1,0.,1.,0.);
+  vec3.set(axis2,1.,0.,0.);
+  mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, 1.0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, time*0.13, axis2 );
+  mat4.rotate(modelViewMatrix, modelViewMatrix, time*0.1, axis1 );
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
+  gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix,false, projectionMatrix);
+    gl.uniformMatrix4fv(programInfo.uniformLocations.cameraMatrix, false, cameraMatrix);
   {
-    const numComponents = 2;
+    const numComponents = 3;
     const type = gl.FLOAT;
     const normalize = false;
     const stride = 0;
@@ -136,20 +139,10 @@ function drawScene(gl, programInfo, buffers) {
         programInfo.attribLocations.vertexPosition);
   }
 
-  gl.useProgram(programInfo.program);
-
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
-
+    gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
   {
     const offset = 0;
-    const vertexCount = 4;
+    const vertexCount = 8;
     gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
   }
 }
