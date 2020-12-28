79a2cfcc6b21ef3ad561f9deeec1dfff1b3d1ea4
Gavin Stark <atthecodeface@gmail.com>
2020-12-28 11:56:02 +0000

    [mesh] Added Mesh and Submesh

--- object.js
+++ object.js
@@ -1,5 +1,92 @@
-function initBuffers(gl) {
+class Submesh {
+    constructor(bone_indices, gl_type, offset, count) {
+        this.bone_indices = bone_indices;
+        this.gl_type = gl_type;
+        this.vindex_offset = offset
+        this.vindex_count = count;
+    }
+}
+class Mesh {
+    //f constructor
+    constructor(gl, positions, weights, indices, submeshes) {
+        this.positions = gl.createBuffer();
+        this.weights   = gl.createBuffer();
+        this.indices   = gl.createBuffer();
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positions);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.weights);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(weights), gl.STATIC_DRAW);
 
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+        this.submeshes = submeshes;
+    }
+    draw(gl, shader, bones) {
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER,         this.positions);
+        gl.enableVertexAttribArray(shader.attribLocations.vertexPosition);
+        gl.vertexAttribPointer(shader.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER,         this.weights);
+        gl.enableVertexAttribArray(shader.attribLocations.vertexWeights);
+        gl.vertexAttribPointer(shader.attribLocations.vertexWeights, 4, gl.FLOAT, false, 0, 0);
+
+        const mymatrix = Array(64);
+        const gl_types = {"TS":gl.TRIANGLE_STRIP};
+        for (const sm of this.submeshes) {
+            for (var i=0; i<16; i++) {
+                mymatrix[ 0+i] = bones[sm.bone_indices[0]].animated_mtm[i];
+                mymatrix[16+i] = bones[sm.bone_indices[1]].animated_mtm[i];
+                mymatrix[32+i] = bones[sm.bone_indices[2]].animated_mtm[i];
+                mymatrix[48+i] = bones[sm.bone_indices[3]].animated_mtm[i];
+            }
+            gl.uniformMatrix4fv(shader.uniformLocations.boneMatrices, false, mymatrix);    
+            gl.drawElements(gl_types[sm.gl_type], sm.vindex_count, gl.UNSIGNED_BYTE, sm.vindex_offset);
+        }
+    }
+}
+
+//a Cube mesh with single bone
+// cube front face   back face (as seen from front)
+//        1    0        5   4
+//        3    2        7   6
+// triangles (anticlockwise for first)
+//  3.2.1 2.1.0 1.0.4 0.4.2 4.2.6 2.6.7 6.7.4 7.4.5 4.5.1 5.1.7 1.7.3 7.3.2
+// Cube strip
+// 3, 2, 1, 0, 4, 2, 6, 7, 4, 5, 1, 7, 3, 2
+const cube =  {
+    positions : [
+      1.0,  1.0, 1.0, 
+      -1.0,  1.0, 1.0,
+      1.0, -1.0, 1.0, 
+      -1.0, -1.0, 1.0,
+
+      1.0,  1.0, -1.0,
+      -1.0,  1.0, -1.0,
+      1.0, -1.0, -1.0,
+      -1.0, -1.0, -1.0,
+
+    ],
+    weights : [
+      1., 0., 0., 0.,
+      1., 0., 0., 0.,
+      1., 0., 0., 0.,
+      1., 0., 0., 0.,
+      0., 1., 0., 0.,
+      0., 1., 0., 0.,
+      0., 1., 0., 0.,
+      0., 1., 0., 0.,
+    ],
+    indices :  [3, 2, 1, 0, 4, 2, 6, 7,  4, 5, 1, 7, 3, 2],
+    submeshes : [ new Submesh([0,1,0,0], "TS", 0, 14),
+                ],
+}
+
+//a Double cube object
     // cube front face   back face (as seen from front)
     //        1    0        5   4
     //        3    2        7   6
@@ -9,7 +96,8 @@ function initBuffers(gl) {
     // 3, 2, 1, 0, 4, 2, 6, 7, 4, 5, 1, 7, 3, 2
     // Double cube strip
     // 3, 2, 1, 0, 4, 2, 6, 7,   11, 5, 9, 8, 11, 10, 6, 8,   4, 5, 1, 7, 3, 2
-  const positions = [
+const dbl_cube =  {
+    positions : [
       1.0,  1.0, 1.0, 
       -1.0,  1.0, 1.0,
       1.0, -1.0, 1.0, 
@@ -24,8 +112,8 @@ function initBuffers(gl) {
       -1.0,  1.0, -3.0,
       1.0, -1.0, -3.0,
       -1.0, -1.0, -3.0,
-  ];
-  const weights = [
+    ],
+    weights : [
       1., 0., 0., 0.,
       1., 0., 0., 0.,
       1., 0., 0., 0.,
@@ -38,21 +126,19 @@ function initBuffers(gl) {
       0., 0., 1., 0.,
       0., 0., 1., 0.,
       0., 0., 1., 0.,
-  ];
-    const indices = [3, 2, 1, 0, 4, 2, 6, 7,   11, 5, 9, 8, 11, 10, 6, 8,   4, 5, 1, 7, 3, 2];
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  const weightBuffer  = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, weightBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(weights), gl.STATIC_DRAW);
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
-  return {
-    position: positionBuffer,
-    weight:   weightBuffer,
-    index:    indexBuffer,
-  };
+    ],
+    indices :  [3, 2, 1, 0, 4, 2, 6, 7,   11, 5, 9, 8, 11, 10, 6, 8,   4, 5, 1, 7, 3, 2],
+    submeshes : [ new Submesh([0,1,2,0], "TS", 0, 22),
+                ],
 }
 
+function draw_objects(gl, shader, meshes, bones, matrices) {
+    gl.useProgram(shader.program);
+    gl.uniformMatrix4fv(shader.uniformLocations.projectionMatrix,false, matrices[0]);
+    gl.uniformMatrix4fv(shader.uniformLocations.cameraMatrix, false, matrices[1]);
+    gl.uniformMatrix4fv(shader.uniformLocations.modelMatrix, false, matrices[2]);
+
+    for (const m of meshes) {
+        m.draw(gl, shader, bones);
+    }
+}
--- webgl.js
+++ webgl.js
@@ -10,12 +10,14 @@ function main() {
     }
 
     shaders = shader_compile(gl);
-    const buffers = initBuffers(gl);
-    run_animation(gl, shaders, buffers);
+    const meshes = []
+    meshes.push(new Mesh(gl, dbl_cube.positions, dbl_cube.weights, dbl_cube.indices, dbl_cube.submeshes));
+    meshes.push(new Mesh(gl, cube.positions, cube.weights, cube.indices, cube.submeshes));
+    run_animation(gl, shaders, meshes);
 }
 
 var time=0.0;
-function run_animation(gl, shaders, buffers) {
+function run_animation(gl, shaders, meshes) {
     a = new Bone(null);
     b = new Bone(a);
     c = new Bone(b);
@@ -26,87 +28,59 @@ function run_animation(gl, shaders, buffers) {
     a.derive_at_rest();
     a.derive_animation();
     bones = [a,b,c];
-    step_animation = function() {drawScene(gl, shaders, buffers, time, bones); time+=0.1; requestAnimationFrame(step_animation);}
+
+    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    const cameraMatrix = mat4.create();
+    mat4.translate(cameraMatrix,     // destination matrix
+                   cameraMatrix,     // matrix to translate
+                   [-0.0, 0.0, -20.0]);  // amount to translate
+    matrices = [projectionMatrix, cameraMatrix, mat4.create()];
+    step_animation = function() {
+        drawScene(gl, shaders, meshes, time, bones, matrices);
+        time+=0.1;
+        requestAnimationFrame(step_animation);
+    }
     requestAnimationFrame(step_animation);
 }
 
-function drawScene(gl, shaders, buffers, time, bones) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+function drawScene(gl, shaders, meshes, time, bones, matrices) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
     gl.enable(gl.CULL_FACE);
     gl.cullFace(gl.BACK);
     //gl.cullFace(gl.FRONT);
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  const fieldOfView = 45 * Math.PI / 180;   // in radians
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-
-  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-  const cameraMatrix = mat4.create();
-  mat4.translate(cameraMatrix,     // destination matrix
-                 cameraMatrix,     // matrix to translate
-                 [-0.0, 0.0, -40.0]);  // amount to translate
-
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
-  gl.useProgram(shaders[0].program);
-
-  gl.uniformMatrix4fv(shaders[0].uniformLocations.projectionMatrix,false, projectionMatrix);
-  gl.uniformMatrix4fv(shaders[0].uniformLocations.cameraMatrix, false, cameraMatrix);
-
-    const mymatrix = Array(64);
     const q = quat.create();
     const angle=Math.sin(time*0.2)*0.3;
     quat.identity(q);
+    quat.rotateX(q,q,1.85);
+    //quat.rotateX(q,q,+angle*2);
     bones[0].translate_from_rest(vec3.set(vec3.create(),0.,0.,0.));
     bones[0].quaternion_from_rest(q);
     quat.identity(q);
     quat.rotateZ(q,q,4*angle);
-    bones[1].translate_from_rest(vec3.set(vec3.create(),0.,0.,-angle*0.9));
+    bones[1].translate_from_rest(vec3.set(vec3.create(),0.,0.,1.0-Math.cos(4*angle)));
     bones[1].quaternion_from_rest(q);
     quat.identity(q);
     quat.rotateZ(q,q,-4*angle);
-    bones[2].translate_from_rest(vec3.set(vec3.create(),0.,0.,+angle*0.9));
+    bones[2].translate_from_rest(vec3.set(vec3.create(),0.,0.,1.0-Math.cos(4*angle)));
     bones[2].quaternion_from_rest(q);
     bones[0].derive_animation();
-    mat4.copy(mymatrix, bones[0].animated_mtm);
-    for (var i=0; i<16; i++) {
-        mymatrix[ 0+i] = bones[0].animated_mtm[i];
-        mymatrix[16+i] = bones[1].animated_mtm[i];
-        mymatrix[32+i] = bones[2].animated_mtm[i];
-    }
-  gl.uniformMatrix4fv(shaders[0].uniformLocations.boneMatrices, false, mymatrix);    
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.index);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER,         buffers.position);
-  gl.enableVertexAttribArray(shaders[0].attribLocations.vertexPosition);
-  gl.vertexAttribPointer(shaders[0].attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER,         buffers.weight);
-  gl.enableVertexAttribArray(shaders[0].attribLocations.vertexWeights);
-  gl.vertexAttribPointer(shaders[0].attribLocations.vertexWeights, 4, gl.FLOAT, false, 0, 0);
-
-  const axis1 = vec3.create();
-  const axis2 = vec3.create();
-  vec3.set(axis1,0.,1.,0.);
-  vec3.set(axis2,1.,0.,0.);
-  var modelMatrix = mat4.create();
-  const scale = vec4.create();
-  vec3.set(scale,2.5,2.5,2.5,1.0);
-  mat4.translate(modelMatrix, modelMatrix, [3.0, 0.0, 0.0]);
-  mat4.rotate(modelMatrix, modelMatrix, time*0.13, axis2 );
-  mat4.rotate(modelMatrix, modelMatrix, time*0.10, axis1 );
-    mat4.scale(modelMatrix, modelMatrix, scale);
-  gl.uniformMatrix4fv(shaders[0].uniformLocations.modelMatrix, false, modelMatrix);
-  gl.drawElements(gl.TRIANGLE_STRIP, 22, gl.UNSIGNED_BYTE, 0);
+    mat4.identity(matrices[2]);
+    mat4.translate(matrices[2], matrices[2], [3.0, 0.0, 0.0]);
+    draw_objects(gl, shaders[0], meshes, bones, matrices);
 }
 
 main();
