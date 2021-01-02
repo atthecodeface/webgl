c5c223cfad1370b3ea28ce86cbcd1d8125d2e292
Gavin Stark <atthecodeface@gmail.com>
2021-01-02 15:46:31 +0000

    [js] Improved testing

--- gjsgl/test.js
+++ gjsgl/test.js
@@ -1,16 +1,21 @@
-function assert_eq(a,b,msg) {
-    if (a!=b) {
-        console.log("ASSERTION FAILED: ",a,b,msg);
+var errors = 0;
+var asserts = 0;
+function assert(a,...args) {
+    asserts++;
+    if (!a) {
+        errors++;
+        console.log("ASSERTION FAILED: ",...args);
     }
 }
-function assert_vec_eq(a,b,msg) {
-    if ((a[0]!=b[0]) || (a[1]!=b[1]) || (a[2]!=b[2])) {
-        console.log("ASSERTION FAILED: ",a,b,msg);
-    }
+function assert_eq(a,b,...args) {
+    assert(a==b,...args);
+}
+function assert_vec_eq(a,b,...args) {
+    assert ((a[0]==b[0]) && (a[1]==b[1]) && (a[2]!=b[2]), a,b,...args);
 }
 function assert_trans_eq(t,scale,trans,quat,msg) {
-    assert_vec_eq(t.scale,      scale,msg+"Scale");
-    assert_vec_eq(t.translation,trans,msg+"Trans");
+    const t2 = new Transformation(translation=trans,quaternion=quat,scale=scale);
+    assert(t.distance(t2)<1E-5,msg,"transform not equal enough : actual, expected :", t, t2);
 }
 
 function test_transformation() {
@@ -22,38 +27,52 @@ function test_transformation() {
                                  scale=b.scale);
     const d = new Transformation();
 
-    assert_trans_eq(a,[1.,1.,1.],[0.,0.,0.],0,"a");
-    assert_trans_eq(b,[1.,1.,1.],[0.,0.,0.],0,"b");
-    assert_trans_eq(c,[1.,1.,1.],[1.,2.,3.],0,"c");
+    const q = quat.create();
+    assert_trans_eq(a,[1.,1.,1.],[0.,0.,0.],q,"a");
+    assert_trans_eq(b,[1.,1.,1.],[0.,0.,0.],q,"b");
+    assert_trans_eq(c,[1.,1.,1.],[1.,2.,3.],q,"c");
     d.set(a,c);
-    assert_trans_eq(d,[1.,1.,1.],[1.,2.,3.],0,"d");
+    assert_trans_eq(d,[1.,1.,1.],[1.,2.,3.],q,"d");
     d.set(d,c);
-    assert_trans_eq(d,[1.,1.,1.],[2.,4.,6.],0,"d2");
+    assert_trans_eq(d,[1.,1.,1.],[2.,4.,6.],q,"d2");
+
+    const ma = new Transformation(translation=vec3.fromValues(4.,5.,6.),
+                                  quaternion=quat.setAxisAngle(quat.create(),vec3.fromValues(1.,0.,0.),0.1),
+                                  scale=vec3.fromValues(7.,8.,9.));
+    const mb = new Transformation();
+    mb.from_mat4(ma.mat4());
+    assert(mb.distance(ma)<1E-5, "transformation to and from mat4")
 }
 
 function test_bone() {
     const a = new Bone();
     const b = new Bone(a);
+    const q = quat.create();
     assert_eq(a,b.parent,"b parent");
-    assert_trans_eq(a.transformation,[1.,1.,1],[0.,0.,0.],0,"bone a trans");
+    assert_trans_eq(a.transformation,[1.,1.,1],[0.,0.,0.],q,"bone a trans");
+    a.transform(new Transformation([1.,2.,3.],q,[3.,-1.,-2.]));
+    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],q,"bone a trans");
     a.transform(new Transformation([1.,2.,3.],undefined,[3.,-1.,-2.]));
-    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],0,"bone a trans");
-    a.transform(new Transformation([1.,2.,3.],undefined,[3.,-1.,-2.]));
-    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],0,"bone a trans");
-    assert_trans_eq(a.transformation_rest,[1.,1.,1],[0.,0.,0.],0,"bone a trans rest");
+    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],q,"bone a trans");
+    assert_trans_eq(a.transformation_rest,[1.,1.,1],[0.,0.,0.],q,"bone a trans rest");
     a.derive_at_rest();
-    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],0,"bone a trans after at rest");
-    assert_trans_eq(a.transformation_rest,[9.,1.,4],[2.,4.,6.],0,"bone a trans rest after at rest");
+    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],q,"bone a trans after at rest");
+    assert_trans_eq(a.transformation_rest,[9.,1.,4],[2.,4.,6.],q,"bone a trans rest after at rest");
     a.transform(new Transformation([-1.,-2.,-3.],undefined,[1./3.,-1.,-1./2.]));
-    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],0,"bone a trans again");
+    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],q,"bone a trans again");
     a.transform(new Transformation(undefined,undefined,undefined));
-    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],0,"bone a trans again v2");
+    assert_trans_eq(a.transformation,[3.,-1.,-2],[1.,2.,3.],q,"bone a trans again v2");
     a.transform_from_rest(new Transformation(undefined,undefined,undefined));
-    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],0,"bone a trans again v3");
+    assert_trans_eq(a.transformation,[9.,1.,4],[2.,4.,6.],q,"bone a trans again v3");
 }
 
 function main() {
     test_transformation();
     test_bone();
+    console.log("Hit ",asserts," assertions");
+    if (errors>0) {
+        console.log("FAILED WITH",errors," errors");
+        error;
+    }
 }
 main();
--- gjsgl/transformation.js
+++ gjsgl/transformation.js
@@ -1,3 +1,40 @@
+// quaternion_of_rotation
+function quaternion_of_rotation(rotation) {
+    const rot_min_id   = mat3.subtract(mat3.create(), rotation, mat3.multiplyScalar(mat3.create(),mat3.create(),0.99999));
+    const rot_min_id_i = mat3.invert(mat3.create(), rot_min_id);
+    var axis = vec3.create();
+    for (var j=0; j<3; j++) {
+        var v = vec3.create();
+        var last_v = vec3.copy(vec3.create(),v);
+        v[j] = 1.;
+        for (i=0; i<10; i++) {
+            vec3.copy(last_v,v);
+            vec3.normalize(v,vec3.transformMat3(v,v,rot_min_id_i));
+        }
+        vec3.copy(axis,v);
+        const dist2 = vec3.sqrLen(vec3.subtract(v,v,last_v));
+        if (dist2<0.00001) {break;}
+    }
+
+    var w = vec3.fromValues(1.0,0,0);
+    if ((axis[0]>0.9) || (axis[0]<-0.9)) {w = vec3.fromValues(0,1.0,0);}
+    const na0 = vec3.normalize(vec3.create(), vec3.cross(vec3.create(), w, axis));
+    const na1 = vec3.cross(vec3.create(), axis, na0);
+
+    // Rotate w_perp_n around the axis of rotation by angle A
+    const na0_r = vec3.transformMat3(vec3.create(), na0, rotation);
+    const na1_r = vec3.transformMat3(vec3.create(), na1, rotation);
+
+    // Get angle of rotation
+    const cos_angle =  vec3.dot(na0, na0_r);
+    const sin_angle = -vec3.dot(na0, na1_r);
+    const angle = Math.atan2(sin_angle, cos_angle);
+
+    // Set quaternion
+    return quat.setAxisAngle(quat.create(), axis, angle);
+}
+
+//a Transformation
 class Transformation {
     constructor(translation, quaternion, scale) {
         if (translation === undefined) {
@@ -19,11 +56,13 @@ class Transformation {
             vec3.copy(this.scale,scale);
         }
     }
+    //f copy
     copy(other) {
         quat.copy(this.quaternion,  other.quaternion);
         vec3.copy(this.translation, other.translation);
         vec3.copy(this.scale,       other.scale);
     }
+    //f set
     set(base, other) {
         quat.multiply(this.quaternion, base.quaternion, other.quaternion);
         vec3.add(this.translation, base.translation, other.translation);
@@ -31,6 +70,7 @@ class Transformation {
             this.scale[i] = base.scale[i] * other.scale[i];
         }
     }
+    //f mat4
     mat4() {
         var m = mat4.create();
         mat4.fromQuat(m, this.quaternion);
@@ -43,6 +83,29 @@ class Transformation {
             m[4*i+2] *= this.scale[i];
         }
         return m;
+    }
+    //f from_mat4
+    from_mat4(m) {
+        this.translation = vec3.fromValues(m[3*4+0], m[3*4+1], m[3*4+2]);
+        var rotation = mat3.create();
+        for (var i=0; i<3; i++) {
+            const v = vec3.fromValues(m[4*i+0],m[4*i+1],m[4*i+2]);
+            const l = vec3.length(v);
+            this.scale[i] = l;
+            rotation[3*i+0] = v[0]/l;
+            rotation[3*i+1] = v[1]/l;
+            rotation[3*i+2] = v[2]/l;
+        }
+        this.quaternion = quaternion_of_rotation(rotation);
+    }
+    //f distance
+    distance(other) {
+        const td = vec3.distance(this.translation, other.translation);
+        const sd = vec3.distance(this.scale, other.scale);
+        const qn = quat.multiply(quat.create(),quat.invert(quat.create(),this.quaternion),other.quaternion);
+        if (qn[3]>0) {quat.scale(qn,qn,-1);}
+        const qd = quat.length(quat.add(quat.create(),qn,quat.create()));
+        return td+sd+qd;
     }
 }
 
